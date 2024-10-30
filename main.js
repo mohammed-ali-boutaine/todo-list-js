0df9212ebb57f6d5c0fdce91d4f775ff6af9edff
Mohammed Ali Boutaine <111361086+mohammed-ali-boutaine@users.noreply.github.com>
2024-10-30 14:22:04 +0100

    add dark mode functionality and logic for add task and local storag

--- main.js
+++ main.js
@@ -1,21 +1,130 @@
-import './style.css'
+import "./style.css";
+import "@fortawesome/fontawesome-free/css/all.css";
+import { v4 as uuidv4 } from "uuid";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// dark mode stuff
+// Check and apply the user's stored preference or the system preference on page load
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  document.documentElement.classList.add("dark");
+} else {
+  document.documentElement.classList.remove("dark");
+}
 
-setupCounter(document.querySelector('#counter'))
+// User actions to explicitly choose themes
+function toggleTheme(isDark) {
+  localStorage.theme = isDark ? "dark" : "light"; // Save preference
+  document.documentElement.classList.toggle("dark", isDark);
+}
+
+const darkModeBtn = document.getElementById("dark-mode");
+darkModeBtn.addEventListener("click", () => {
+  const isDark = !document.documentElement.classList.contains("dark");
+  toggleTheme(isDark);
+});
+
+// DOM variables
+const addTaskButton = document.querySelector("#add-task");
+const output = document.querySelector("#output");
+
+// form stuff
+const title = document.getElementById("title");
+const description = document.getElementById("description");
+const status = document.getElementById("status");
+const date = document.getElementById("date");
+const time = document.getElementById("time");
+const priorite = document.getElementById("priorite");
+
+// tasks variable
+let tasks = localStorage.getItem("tasks");
+
+// get tasks from local storag and handl it
+if (tasks == "" || tasks == null) {
+  tasks = [
+    {
+      id: "1",
+      title: "gym",
+      description: "go to the gym and worout legs",
+      status: "todo",
+      date: "2024-10-29",
+      time: "12:55:44",
+      priorite: "p1",
+    },
+  ];
+} else {
+  tasks = JSON.parse(tasks);
+}
+
+// add tasks precedure
+function addTask() {
+  const task = {
+    id: uuidv4(),
+    title: title.value,
+    description: description.value,
+    status: status.value,
+    date: date.value,
+    time: time.value,
+    priorite: priorite.value,
+  };
+
+  title.value = "";
+  description.value = "";
+  status.value = "";
+  date.value = "";
+  time.value = "";
+  priorite.value = "";
+
+  tasks.push(task);
+  //   tasks = [...tasks , task]
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+
+  showData();
+  //   console.log(tasks);
+}
+// function to delete all
+function deleteAll() {
+  tasks = [];
+  localStorage.removeItem("tasks");
+}
+
+function deleteOne(id) {
+  console.log(id);
+  console.log("tetx");
+
+  tasks = tasks.filter((task) => task.id != id);
+  showData();
+}
+// show data
+function showData() {
+  const ul = document.createElement("li");
+  console.log(tasks);
+
+  if (tasks.length !== 0) {
+    ul.innerHTML = tasks.map((task) => {
+      return `
+                <li class="w-56 bg-cyan-950  task-item flex flex-col justify-between items-center p-4 border rounded-md shadow-sm">
+                <div>
+                  <h3 class="text-lg font-semibold">${task.title}</h3>
+                  <p class="text-gray-600">${task.description}</p>
+                  <p class="text-sm text-gray-500">Due: ${task.time} ${task.date}</p>
+                  <p class="text-sm text-gray-500">Priority: ${task.priorite}</p>
+                  <p class="text-sm text-gray-500">status: ${task.status}</p>
+                </div>
+                <div class="flex gap-2">
+                  <button class="text-white bg-green-500 hover:bg-green-600  px-3 py-1 rounded-md">Edit</button>
+                  <button onclick=' deleteOne( ${task.id} )' class="text-white bg-red-500 hover:bg-red-600 px-3 py-1 rounded-md">Delete</button>
+                </div>
+              </li>
+                `;
+    });
+    output.appendChild(ul);
+  } else {
+    ul.innerHTML = `<div>nothing to show</div>`;
+  }
+}
+// add events
+// addTaskButton.addEventListener("click", addTask);
+// showData();
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'selector', // Enable selector strategy for dark mode
   content: ["./*.{html,js}"],
   theme: {
     extend: {},
